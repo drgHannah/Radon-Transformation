7a2289ad9105d2d383253d1c2cb2a3d9f3e27703
Hannah <drg.hannah@gmail.com>
2021-04-23 13:34:49 +0200

    fixed bugs

--- radon.py
+++ radon.py
@@ -9,7 +9,6 @@ import torch
 import numpy as np
 
 # for dataset
-import dival
 from dival import get_standard_dataset
 
 # for displaying
@@ -19,29 +18,25 @@ class radon(torch.nn.Module):
     '''
     Radon Transformation
     '''
-    def forward(self, input, settings):
-        with torch.no_grad():
-            n_angles   = settings["n_angles"]
-            det_count  = settings["det_count"]
-            image_size = settings["image_size"]
-            step_size = settings["step_size"]
-            sampling_points = settings["sampling_points"]
-            batchify_rays = settings["batchify_rays"]
-   
-        sample_points = self.get_sample_points(n_angles, det_count, image_size, step_size, sampling_points)
+    def __init__(self, n_angles, det_count, image_size, step_size=1, sampling_points=100, batchify_rays=[], device='cuda'):
+        super(radon, self).__init__()
+        self.sample_points = self.get_sample_points(n_angles, det_count, image_size, step_size, sampling_points)
+        self.batchify_rays = batchify_rays
+        self.device = device
 
-        n_steps, n_dims, n_angles, n_count = sample_points.shape
-        sample_points = sample_points.reshape([n_steps, n_dims, -1])
-        if len(batchify_rays)>0:
-            sample_points = sample_points[:,:,batchify_rays]
+    def forward(self, input):
+        n_steps, n_dims, n_angles, n_count = self.sample_points.shape
+        sample_points = self.sample_points.reshape([n_steps, n_dims, -1])
+        if len(self.batchify_rays)>0:
+            sample_points = sample_points[:,:,self.batchify_rays]
         sample_points = sample_points.unsqueeze(-1)
-        out = self.intensity_from_rays(sample_points, input)
+        out = self.intensity_from_rays(sample_points, input, self.device)
         out = torch.rot90(out, 2)
 
         out = out.view(n_angles, n_count)
         return out
 
-    def intensity_from_rays(self,sample_points, img):
+    def intensity_from_rays(self,sample_points, img, device):
         '''
         Calculates Intenity I_d of Radon transformation: I_d = exp(sum of pixel at alpha shift: u(x)){*I_0}
         Args:
@@ -53,15 +48,13 @@ class radon(torch.nn.Module):
         img_n = img[None, None]
         grid = sample_points.permute(0,2,3,1)
         steps, angles, shifts, axis = grid.shape
-        grid = grid.reshape(1,-1,1,2)
-        out = torch.nn.functional.grid_sample(img_n, grid.cuda(), padding_mode='zeros', align_corners=True)
+        grid = grid.reshape(1,-1,1,2).to(device)
+        out = torch.nn.functional.grid_sample(img_n, grid, padding_mode='zeros', align_corners=True)
         out = out.view(steps, angles, shifts)
         intens = torch.sum(out, dim = 0) / out.shape[0]
-        
-
         return intens
 
-    def get_sample_points(self, n_angles, det_count, image_size, step_size = 1, sampling_points = 100):
+    def get_sample_points(self, n_angles, det_count, image_size, step_size=1, sampling_points=100):
         ''' 
         Calculates the samples, sampled by radon transformation.
         Args:
@@ -78,14 +71,12 @@ class radon(torch.nn.Module):
         sx = sx * det_count / 2 * distance
         sx = sx * step_size
 
-
         sy = -np.sqrt(2)
 
         ex = sx
         ey = -sy
 
         angles = torch.linspace(0, np.pi, n_angles).unsqueeze(-1) + (np.pi / 2)
-
 
         rsx = sx * torch.cos(angles) - sy * torch.sin(angles) # start x
         rsy = sx * torch.sin(angles) + sy * torch.cos(angles) # start y
@@ -112,31 +103,23 @@ class radon(torch.nn.Module):
 
 if __name__=='__main__':
 
+    device = 'cuda'
 
     # Load dataset
     dataset = get_standard_dataset('lodopab')
     data= dataset.create_torch_dataset('train')
     idx = 0
-    fd_reconstruction = torch.tensor(data[idx][1],device='cuda')
-    ld_sinogram = torch.tensor(data[idx][0],device='cuda')
-
-    # Transformation settings
-    settings = {
-        "n_angles"   : 1000,
-        "det_count"  : 500,
-        "image_size" : fd_reconstruction.shape[0],
-        "step_size" : 1,
-        "sampling_points" : 100,
-        "batchify_rays": [],
-    }
-
+    fd_reconstruction = torch.tensor(data[idx][1]).to(device)
+    ld_sinogram =  torch.tensor(data[idx][0]).to(device)
 
     # Apply radon
-    radon_op = radon()
-    out = radon_op(fd_reconstruction,settings)
+    radon_op = radon(1000, 500, fd_reconstruction.shape[0],device=device)
+    out = radon_op(fd_reconstruction)
 
     print("Input: ", fd_reconstruction.shape)
     print("Radon Transform: ", out.shape)
 
     plt.imshow(out.cpu())
     plt.savefig('output.png')
+    plt.close()
+    
